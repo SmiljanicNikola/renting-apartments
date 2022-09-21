e4b356de4d5c509aa813caae9e78181aa2f49e42
Nikoa <nikola.kiki.00@gmail.com>
2022-09-21 20:57:58 +0200

    Started spring profiles, added new app properties files for different app environments

--- Backend/RentingApartments/src/main/java/com/example/RentingApartments/service/implementation/ClientServiceImpl.java
+++ Backend/RentingApartments/src/main/java/com/example/RentingApartments/service/implementation/ClientServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.RentingApartments.repository.AdvertisementRepository;
 import com.example.RentingApartments.repository.ClientRepository;
 import com.example.RentingApartments.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
